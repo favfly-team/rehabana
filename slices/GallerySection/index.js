436e6bcb50dd8bc852df7dbb826093ea7c39ae6c
FavCoder <darpan@favfly.co>
2026-02-14 13:51:35 +0530

    Remove console log from GallerySection for cleaner code

--- slices/GallerySection/index.js
+++ slices/GallerySection/index.js
@@ -3,7 +3,6 @@ import DefaultSection from "@/components/gallery-section";
 const GallerySection = ({ slice, context }) => {
   switch (slice.variation) {
     case "global": {
-      console.log(context.globalGallery?.data?.default);
       return (
         <DefaultSection
           slice={{ primary: context.globalGallery?.data?.default?.[0] }}
